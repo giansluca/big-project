98ed8f76605fef4c42e5a54448e1ec54f1c7d211
Gian Luca Mori <g.mori@teamsystem.com>
2025-01-24 17:33:17 +0100

    update message

--- barbero/src/main/java/com/gmdev/barbero/Main.java
+++ barbero/src/main/java/com/gmdev/barbero/Main.java
@@ -3,7 +3,8 @@ package com.gmdev.barbero;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Barbero v0.01");
+        System.out.println("Hello");
+        System.out.println("Hello Barbero top");
     }
 
 }
--- pepe/src/main/java/com/gmdev/pepe/Main.java
+++ pepe/src/main/java/com/gmdev/pepe/Main.java
@@ -3,7 +3,8 @@ package com.gmdev.pepe;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Pepe v0.01");
+        System.out.println("Hello");
+        System.out.println("Hello Pepe top");
     }
 
 }
