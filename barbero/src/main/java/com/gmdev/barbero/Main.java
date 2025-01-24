7a926a8f044897d1df6ebee6a423b0517c172070
Gian Luca Mori <g.mori@teamsystem.com>
2025-01-24 16:35:50 +0100

    add new hello message on modules

--- barbero/src/main/java/com/gmdev/barbero/Main.java
+++ barbero/src/main/java/com/gmdev/barbero/Main.java
@@ -3,7 +3,7 @@ package com.gmdev.barbero;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Barbero");
+        System.out.println("Hello Barbero v0.02");
     }
 
 }
--- pepe/src/main/java/com/gmdev/pepe/Main.java
+++ pepe/src/main/java/com/gmdev/pepe/Main.java
@@ -3,7 +3,7 @@ package com.gmdev.pepe;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Pepe");
+        System.out.println("Hello Pepe v0.02");
     }
 
 }
